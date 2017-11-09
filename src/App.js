e8e81585993f7b0d736bf6e6e89283aa41bcedee
George Melidis <melidis@gmail.com>
2017-11-09 23:04:20 +0200

    test completed

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <footer className="App-footer">
+            <p>
+                Loftschool
+            </p>
+        </footer>
       </div>
     );
   }
